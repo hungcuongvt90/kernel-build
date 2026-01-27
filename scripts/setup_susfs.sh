47a6a0014d5086f8d4ea2da38cd95101234c8e04
Cuong Nguyen <cuongnguyen@era.com.sg>
2026-01-27 11:32:10 +0700

    Test

--- scripts/setup_susfs.sh
+++ scripts/setup_susfs.sh
@@ -26,15 +26,15 @@ elif [ "$kernelsu_variant" == "SukiSU" ]; then
 
   patch -p1 < 50_add_susfs_in_${SUSFS_BRANCH}.patch || true
 
-  if [ "${HOOK_VARIANT}" == "scope_min_manual" ]; then
-    echo "Apply scope min manual hook patches"
-    cp ../SukiSU_patch/hooks/scope_min_manual_hooks_v1.6.patch ./
-    patch -p1 -F 3 < scope_min_manual_hooks_v1.6.patch
-  else
-    echo "Apply syscall hook patches"
-    cp ../SukiSU_patch/hooks/syscall_hooks.patch ./
-    patch -p1 -F 3 < syscall_hooks.patch
-  fi
+  # if [ "${HOOK_VARIANT}" == "scope_min_manual" ]; then
+  #   echo "Apply scope min manual hook patches"
+  #   cp ../SukiSU_patch/hooks/scope_min_manual_hooks_v1.6.patch ./
+  #   patch -p1 -F 3 < scope_min_manual_hooks_v1.6.patch
+  # else
+  #   echo "Apply syscall hook patches"
+  #   cp ../SukiSU_patch/hooks/syscall_hooks.patch ./
+  #   patch -p1 -F 3 < syscall_hooks.patch
+  # fi
 else
   echo "Invalid KernelSU variant selected!"
   exit 1
